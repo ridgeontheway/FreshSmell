882a25171821c015e45eb758401b46615e72aa30
George <george.ridgay@protonmail.com>
2019-03-05 14:43:19 +0000

    test

--- src/main/java/org/wasps/model/fromSourceCode/ParsedInterface.java
+++ src/main/java/org/wasps/model/fromSourceCode/ParsedInterface.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class ParsedInterface extends SourceCodeBase {
 
-    public ParsedInterface(File fileRef){
+    public ParsedInterface(File fileRef){ //test
         super(fileRef);
     }
 
