f81805c8bc9070461f1fcb278500fee93aa6cc3c
OsosKP <ososkp@gmail.com>
2019-04-14 11:20:44 +0100

    Restructured code, added some CSS, got rid of unnecessary methods

--- src/main/java/org/wasps/controller/FileUploadController.java
+++ src/main/java/org/wasps/controller/FileUploadController.java
@@ -2,7 +2,6 @@ package org.wasps.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,15 +33,5 @@ public class FileUploadController extends BaseController {
     public @ResponseBody
     String uploadFiles(@RequestParam("file") MultipartFile[] files) {
         return _worker.mapper().mapFiles(request, files);
-    }
-
-    @RequestMapping(value = "/upload", method = RequestMethod.GET)
-    public String getUpload(Model model) {
-        return "upload";
-    }
-
-    @RequestMapping(value = "/uploadMultiple", method = RequestMethod.GET)
-    public String getUploadMultiple(Model model) {
-        return "uploadMultiple";
     }
 }
